bfd4d31d72064528786d850dcf94da3bff35b215
Vinicius Miranda de Araujo <vinicius.araujo.1457564@sga.pucminas.br>
2025-03-21 22:26:39 -0300

    feat: página home, no momento utilizada para teste

--- src/lib/screens/home_screen.dart
+++ src/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'utils.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,91 +7,56 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0x00DF9A04),
-        title: const Text("Home"),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      backgroundColor: AppColors.background,
+      appBar: Utils.buildHeader('Home'),
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              "Saldo:",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            // ! TESTE BOTÃO
+            Utils.buildButton(text: "Teste", onPressed: _apertaBotao),
+
+            // ! TESTE INPUT
+            Utils.buildInputField(
+              "Teste",
+              controller: TextEditingController(),
+              type: TextInputType.text,
+              obscure: false,
             ),
-            const Text(
-              "R\$5.500,00",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+
+            Utils.buildInputField(
+              "Senha",
+              controller: TextEditingController(),
+              type: TextInputType.text,
+              obscure: true,
+              width: 300, // Define a largura do campo
             ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                _buildCard("Entrou", "R\$5000,00", Colors.green),
-                _buildCard("Saiu", "R\$2000,00", Colors.red),
-              ],
+
+            // ! TESTE ALERT
+            ElevatedButton(
+              onPressed: () {
+                Utils.showAlertDialog(
+                  context,
+                  "Encerrar Sessão",
+                  "Você tem certeza ?",
+                  onConfirm: _apertaConfirmar,
+                  confirmText: "Sei lá porra kkkkk",
+                  cancelText: "Kero naum",
+                );
+              },
+              child: Text("Teste Alerta"),
             ),
-            const SizedBox(height: 16),
-            Expanded(
-              child: Center(
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  decoration: const BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.circle,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            _buildMetaGastos(),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildCard(String title, String value, Color color) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      width: 150,
-      decoration: BoxDecoration(
-        color: color.withOpacity(0.2),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(title, style: TextStyle(color: color, fontWeight: FontWeight.bold)),
-          Text(value, style: TextStyle(color: color, fontSize: 18)),
-        ],
-      ),
-    );
+  void _apertaBotao() {
+    debugPrint("Botão apertado (lá ele kkkkkkkkkkkkj)");
   }
 
-  Widget _buildMetaGastos() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          "Meta de gastos:",
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        const Text(
-          "- R\$2.500",
-          style: TextStyle(fontSize: 16, color: Colors.red, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 8),
-        LinearProgressIndicator(
-          value: 0.5,
-          backgroundColor: Colors.grey[300],
-          color: Colors.orange,
-          minHeight: 10,
-        ),
-      ],
-    );
+  void _apertaConfirmar() {
+    debugPrint("Sei lá porra apertado XDXDXD");
   }
 }
